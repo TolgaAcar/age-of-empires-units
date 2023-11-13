6a4d098947759a7fcc411ead74e449098e9212de
TolgaAcar <tolgacarr@gmail.com>
2023-11-14 01:40:56 +0300

    format files with prettier

--- src/main.js
+++ src/main.js
@@ -11,7 +11,6 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import { mdi } from "vuetify/iconsets/mdi";
 import "@mdi/font/css/materialdesignicons.css";
 
 const vuetify = createVuetify({
