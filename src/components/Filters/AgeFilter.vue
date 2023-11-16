c70c54bbed191208ad36e07c4f459a1a06d8a363
TolgaAcar <tolgacarr@gmail.com>
2023-11-16 17:07:15 +0300

    Two way binding between component's selectedAge and state's ageFilter

--- src/components/Filters/AgeFilter.vue
+++ src/components/Filters/AgeFilter.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="age-filter mb-10">
-    <h3 class="mb-5">Ages</h3>
+    <h3 class="mb-5" data-testid="age-filter-title">Ages</h3>
 
     <v-btn
       v-for="(age, index) in ages"
       :key="`age-btn${index}`"
+      :id="`filter-${index}`"
       @click="onAgeButtonClick(age)"
       :color="isButtonSelected(age) ? 'var(--selected-link)' : ''"
       variant="outlined"
@@ -16,35 +17,36 @@
 </template>
 
 <script>
-import { mapActions } from "pinia";
+import { mapActions, mapState } from "pinia";
 import { useUnitsStore } from "../../stores/units";
 import { Ages } from "../../constants/Ages";
 
 export default {
   name: "AgeFilter",
-  data() {
-    return {
-      selectedAge: Ages[0].value
-    };
-  },
   computed: {
     ages() {
       return Ages;
-    }
+    },
+    selectedAge: {
+      set(value) {
+        this.setAgeFilter(value);
+      },
+      get() {
+        return this.ageFilter;
+      }
+    },
+    ...mapState(useUnitsStore, ["ageFilter"])
   },
   methods: {
     onAgeButtonClick(age) {
       this.selectedAge = age.value;
+
+      this.$emit("onAgeButtonClick", this.selectedAge); // Emit the event with the selected age for testing purposes
     },
     isButtonSelected(age) {
       return this.selectedAge === age.value;
     },
     ...mapActions(useUnitsStore, ["setAgeFilter"])
-  },
-  watch: {
-    selectedAge(val) {
-      this.setAgeFilter(val);
-    }
   }
 };
 </script>
